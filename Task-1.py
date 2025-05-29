a5e213eacb12a8b6f9f60a5acf31a179e6c2492b
korakutiReddamma <korakutireddy@gmail.com>
2025-05-29 12:19:41 +0530

    This is first commit

---
+++ Task-1.py
@@ -0,0 +1,15 @@
+# Taking input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Performing basic mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Displaying the results
+print(f"\nAddition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
---
+++ Task-2.py
@@ -0,0 +1,9 @@
+#Takes a user's first name and last name as input.
+firstname=input()
+lastname=input()
+#Concatenates the first name and last name into a full name
+fullname=firstname+lastname+'!'
+#Prints a personalized greeting message using the full name.
+print("Enter your first name:",firstname)
+print("Enter your last name:", lastname)
+print('\nHello,',fullname,'Welcome to the python program')
